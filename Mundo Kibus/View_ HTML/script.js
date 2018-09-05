5ba692856c9701a8e4c5c8ca28a0fd931c3c2c76
sofirob23 <sofirob23@gmail.com>
2018-09-05 08:54:02 -0500

    Slider y tablero verde completos

--- Mundo Kibus/View_ HTML/script.js
+++ Mundo Kibus/View_ HTML/script.js
@@ -1,14 +1,25 @@
 window.onload = function () {
-   var s = Snap(80000, 8000);
+   var s = Snap(800, 600);
+
 //lets draw 2 rects at position 100,100 and then reposition them
     // Cuadricula
     var matrix={};
     for (var i=0;i<15;i++){
         for (var j=0;j<15;j++)
-         matrix [i,j]= s.rect(j*41, i*41, 40, 40, 0, 0);
+
+         matrix [i,j]= s.rect(j*39, i*39, 38, 38, 0, 0).attr('fill','green');
+
 
     }
-    var c = s.circle(20, 594, 10);
-    c.attr('fill', 'red');     //This is the elements area to fill
-console.log(matrix[0,1].getBBox())
+
+    var pos = matrix[0,13].getBBox();
+    console.log(pos);
+    x=39*2;
+    y=39*3;
+    var c = s.image("img/arbol2.png",x, y, 38, 38);
+
+
+
+
+//sconsole.log(matrix[0,1].getBBox())
 }
